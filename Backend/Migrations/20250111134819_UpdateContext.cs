3bb9f9395b34d573689fc1a7862b3626bdc7e391
Junita Berglin <junita.berglin@gmail.com>
2025-01-11 16:16:34 +0100

    🔧 chore: add migration for primary key and precision specification

---
+++ Backend/Migrations/20250111134819_UpdateContext.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateContext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WishListItems_Users_PurchasedByUserId",
+                table: "WishListItems");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "WishListItems",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WishListItems_UserId",
+                table: "WishListItems",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WishListItems_Users_PurchasedByUserId",
+                table: "WishListItems",
+                column: "PurchasedByUserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WishListItems_Users_UserId",
+                table: "WishListItems",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_WishListItems_Users_PurchasedByUserId",
+                table: "WishListItems");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_WishListItems_Users_UserId",
+                table: "WishListItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_WishListItems_UserId",
+                table: "WishListItems");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "WishListItems");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_WishListItems_Users_PurchasedByUserId",
+                table: "WishListItems",
+                column: "PurchasedByUserId",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+    }
+}
